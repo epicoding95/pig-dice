ba21d539e2d5bb3c975a9a9f52485c2691c8c32a
Erik Irgens and kelar crisp <kelar@>
2019-06-19 14:11:59 -0700

    finished version of pigDice

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,22 @@
 $(document).ready(function(){
 
 
-
-  var currentRollValue = 0;
+  var previousRollValue = 1;
+  var currentRollValue = 1;
+  var currentTurnTotal = 0;
   var players = {
     player1Total: 0,
     player2Total: 0
   }
   var activePlayer = 1;
-  var currentTurnTotal = 0;
 
+// This function shows and hides the correct die rolled.
+  function switchDice(currentRollValue){
+
+    $("#dice" + currentRollValue).show();
+    $("#dice" + previousRollValue).hide();
+
+  }
 
   function switchPlayer(currentTurnValue){
     if (activePlayer === 1){
@@ -31,14 +38,24 @@ $(document).ready(function(){
       $("#player2TitleInactive").slideToggle(500);
     }
     currentTurnTotal = 0;
-    $("#player1TotalScore").text(player1Total);
-    console.log("Player 1 Total: " + players.player1Total);
-    console.log("Player 2 Total: " + players.player2Total);
+    $("#player1TotalScore").text(players.player1Total);
+    $("#player2TotalScore").text(players.player2Total);
+
+    if (players.player1Total>= 100){
+      alert("Player 1 Wins")
+
+    } else if (players.player2Total >= 100){
+      alert("Player 2 Wins")
+    }
   }
+
+
 
 // calculate currentTurnTotal for the active player.
 
   $("#rollButton").click(function(){
+    previousRollValue = currentRollValue;
+    console.log("Previous Roll Value is: " + previousRollValue);
     currentRollValue = parseInt(Math.floor(Math.random() * 6) + 1);
     console.log("Current Roll Value is :" + currentRollValue);
     if (currentRollValue === 1) {
@@ -47,8 +64,15 @@ $(document).ready(function(){
     } else if (currentRollValue > 1 && currentRollValue <= 6) {
       currentTurnTotal = currentTurnTotal + currentRollValue;
     }
+    if(activePlayer === 1){
+      $("#player1CurrentScore").text(currentTurnTotal);
+      $("#player2CurrentScore").text("");
+    } else if (activePlayer === 2){
+      $("#player2CurrentScore").text(currentTurnTotal);
+      $("#player1CurrentScore").text("");
+    }
+    switchDice(currentRollValue);
 
-    console.log("Current Turn Total is: " + currentTurnTotal);
     return;
     });
 
